cbde69674466ed1ca674aa705644bc1a99e01d7a
Donnie Plumly <donnie.plumly@gmail.com>
2018-10-12 14:40:33 -0700

    New

--- dotstarCapacitanceControl/dotstarCapacitanceControl.ino
+++ dotstarCapacitanceControl/dotstarCapacitanceControl.ino
@@ -1,53 +1,53 @@
 #include <FastLED.h>
+#include <CapacitiveSensor.h>
 
 // NEOPIXEL DEFINITIONS -----------------------------------------------------------------------------
 #define NUM_LEDS 135
 #define DATA_PIN 5      // Green
-#define CLOCK_PIN 6     // Blue
+#define CLOCK_PIN 6     // Blue    Comment out for Neopixels
 CRGB leds[NUM_LEDS];
 
+
 // BUTTON DEFINITIONS -----------------------------------------------------------------------------
-const int buttonPin = 2;
-long debounceDelay = 98;
-long lastDebounceTime = 0;        // the last time the output pin was toggled
-boolean currentState = LOW;       //stroage for current button state
-boolean lastState = LOW;          //storage for last button state
-boolean neopixelState = LOW;      //storage for the current state of the LED (off/on)
+CapacitiveSensor capSensor = CapacitiveSensor(4, 2);
+int threshold = 1000;
+boolean currentState = LOW;       // stroage for current button state
+boolean lastState = LOW;          // storage for last button state
+boolean neopixelState = LOW;      // storage for the current state of the LED (off/on)
 
 
 void setup() {
-  Serial.begin(9600);             //initialize Serial connection
-
-  // BUTTON SETUP --------------
-  pinMode(buttonPin, INPUT);      //this time we will set the pin as INPUT
+  Serial.begin(9600);                               // initialize Serial connection
+  capSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
 
   // LEDS SETUP --------------
-  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);      // Comment out for Neopixels
+  // FastLED.addLeds<WS2811, DATA_PIN,RGB>(leds, NUM_LEDS);         // Uncomment for Neopixels
   lightsOff();
   FastLED.show();
 }
 
 void loop() {
 
-  currentState = digitalRead(buttonPin);
+  long capValue = capSensor.capacitiveSensor(30);
+  currentState = digitalRead(capValue);
 
-  if ( (millis() - lastDebounceTime) > debounceDelay) {
-
-    if (currentState == HIGH && lastState == LOW) {         //if button has just been pressed
+  if (capValue > threshold) {
+    if (currentState == HIGH && lastState == LOW) {         // if button has just been pressed
       Serial.println("pressed");
-      delay(1);                                 //crude form of button debouncing
+      delay(1);                                             // crude form of button debouncing
 
       //toggle the state of the Neopixels
-      if (neopixelState == HIGH && currentState == HIGH ) {
+
+      if (neopixelState == HIGH) {
         neopixelState = LOW;
         Serial.println("Off");
         lightsOff();
-        lastDebounceTime = millis();          //set the current time
+
       } else {
         neopixelState = HIGH;
         Serial.println("On");
         lightsOn();
-        lastDebounceTime = millis();          //set the current time
       }
     }
     lastState = currentState;
